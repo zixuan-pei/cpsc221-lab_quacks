f4b3b61d7e472c5c602c85a73f49ec0eab7a7b3e
zixuan-pei <peizx28@icloud.com>
2021-02-23 00:10:44 -0800

    hhh

---
+++ cmake-build-debug/CMakeFiles/lab_quacks.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/26335/Desktop/lab_quacks/exercises.cpp" "C:/Users/26335/Desktop/lab_quacks/cmake-build-debug/CMakeFiles/lab_quacks.dir/exercises.cpp.obj"
+  "C:/Users/26335/Desktop/lab_quacks/main.cpp" "C:/Users/26335/Desktop/lab_quacks/cmake-build-debug/CMakeFiles/lab_quacks.dir/main.cpp.obj"
+  "C:/Users/26335/Desktop/lab_quacks/quackfun.cpp" "C:/Users/26335/Desktop/lab_quacks/cmake-build-debug/CMakeFiles/lab_quacks.dir/quackfun.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab_quacks.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_quacks.dir/exercises.cpp.obj"
+  "CMakeFiles/lab_quacks.dir/main.cpp.obj"
+  "CMakeFiles/lab_quacks.dir/quackfun.cpp.obj"
+  "lab_quacks.exe"
+  "lab_quacks.exe.manifest"
+  "lab_quacks.pdb"
+  "liblab_quacks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_quacks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
